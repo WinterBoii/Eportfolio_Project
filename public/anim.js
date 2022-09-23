c8836ba688327618079af7c116784c2882cedfc3
WinterBoii <72822518+WinterBoii@users.noreply.github.com>
2022-09-23 16:57:05 +0200

    dark mode implemented

--- public/anim.js
+++ public/anim.js
@@ -1,19 +1,6 @@
-const scaleFactor = 1 / 20
-let isModalOpen = false;
-let contrastToggle = false;
+let isModalOpen = false
+let contrastToggle = true
 
-function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape")
-  const x = event.clientX * scaleFactor
-  const y = event.clientY * scaleFactor
-
-  for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0
-    const boolInt = isOdd ? -1 : 1
-    // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
-  }
-}
 
 function toggleModal() {
   if (isModalOpen) {
@@ -25,7 +12,7 @@ function toggleModal() {
 }
 
 function toggleContrast() {
-  contrastToggle = !contrastToggle;
+  contrastToggle != contrastToggle;
   if (contrastToggle) {
     document.body.classList += " dark-theme"
   }
@@ -35,25 +22,18 @@ function toggleContrast() {
 }
 
 function contact(event) {
-  event.preventDefault();
-  const loading = document.querySelector(".modal__overlay--loading");
-  const success = document.querySelector(".modal__overlay--success");
-  loading.classList += " modal__overlay--visible";
-  emailjs
-    .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
-      event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
-    )
+  event.preventDefault()
+  const loading = document.querySelector(".modal__overlay--loading")
+  const success = document.querySelector(".modal__overlay--success")
+  loading.classList += " modal__overlay--visible"
     .then(() => {
-      loading.classList.remove("modal__overlay--visible");
-      success.classList += " modal__overlay--visible";
+      loading.classList.remove("modal__overlay--visible")
+      success.classList += " modal__overlay--visible"
     })
     .catch(() => {
-      loading.classList.remove("modal__overlay--visible");
+      loading.classList.remove("modal__overlay--visible")
       alert(
         "The email service is temporarily unavailable. Please contact me directly on email@email.com"
-      );
-    });
+      )
+    })
 }
