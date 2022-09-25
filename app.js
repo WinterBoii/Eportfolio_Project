b7725cf93d84050132daed699c1ebd9475518d80
WinterBoii <72822518+WinterBoii@users.noreply.github.com>
2022-09-25 08:19:51 +0200

    refurbed, modal fixed finally

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const expressSession = require('express-session')
+const SQLiteStore = require('connect-sqlite3')(expressSession)
 const path = require("path");
 const app = express();
 
 const eport = require("./routers/eport");
+
+app.use(expressSession({
+    secret:"winter123",
+    saveUninitialized:false,
+    resave: false,
+    store: new SQLiteStore({
+      database: "sessions.db"
+    })
+}))
 
 app.engine("hbs", expressHandlebars.engine({
 		extname: "hbs",
 		defaultLayout: "main",
 	})
 );
+
+app.get('/login', (req, res) => {
+  if(req.session.isLoggedIn){
+    res.redirect('/')
+    return
+  }
+  model={
+    hideFooter: true
+  }
+  res.render('login.hbs', model)
+  return
+}) 
+
+const ADMIN_USERNAME = "obmu20"
 
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
--- public/anim.js
+++ public/anim.js
@@ -5,10 +5,10 @@ let contrastToggle = true
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
-    return document.body.classList.remove("modal--open");
+    return document.body.classList.remove("modall--open");
   }
   isModalOpen = true;
-  document.body.classList += " modal--open";
+  document.body.classList += " modall--open";
 }
 
 function toggleContrast() {
@@ -23,15 +23,15 @@ function toggleContrast() {
 
 function contact(event) {
   event.preventDefault()
-  const loading = document.querySelector(".modal__overlay--loading")
-  const success = document.querySelector(".modal__overlay--success")
-  loading.classList += " modal__overlay--visible"
+  const loading = document.querySelector(".modall__overlay--loading")
+  const success = document.querySelector(".modall__overlay--success")
+  loading.classList += " modall__overlay--visible"
     .then(() => {
-      loading.classList.remove("modal__overlay--visible")
-      success.classList += " modal__overlay--visible"
+      loading.classList.remove("modall__overlay--visible")
+      success.classList += " modall__overlay--visible"
     })
     .catch(() => {
-      loading.classList.remove("modal__overlay--visible")
+      loading.classList.remove("modall__overlay--visible")
       alert(
         "The email service is temporarily unavailable. Please contact me directly on email@email.com"
       )
